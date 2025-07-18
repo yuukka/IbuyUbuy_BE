274e5e0361f04cd6c93134002dc36d46db4ab54a
sara-lai <shihualai@gmail.com>
2025-07-19 03:27:06 +0800

    adding getChatsForCurrentUser

--- controllers/dms.js
+++ controllers/dms.js
@@ -2,15 +2,32 @@ const User = require("../models/users")
 const Chat = require("../models/dms")
 
 module.exports = {
-    createChat
+    createChat,
+    getChatsForCurrentUser
 }
 
-async function getChatsForCurrentUser(){
+async function getChatsForCurrentUser(req, res){
+    const userId = req.auth.userId
+
+    // todo - not sure if can sort by date with this approach?
+    // trying to get all chats where current User is either user1 or user2   
+    const set1 = await Chat.find({ user_id_1: userId })
+    const set2 = await Chat.find({ user_id_2: userId })
+    const results = [...set1, ...set2]
+
     // trickiness:
     // filter out chats where a) other person started it with current User b) no messages sent yet
+    // todo
+
+    // more problems lol
+    // cant have multiple chats with same person, must be combined....
+
+    res.json({ chats: results })
 }
 
 async function createChat(req, res) {
+    // todo - merge chats with same person, return the old chat instead of the new
+
     // all about user1 and user2 (currentUser & other_user who starting chat with)
     const userId = req.auth.userId
     const other_user_id = req.body.other_user_id // other_user_id is sent through in req.body
--- routes/dms.js
+++ routes/dms.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const dmsCtrl = require("../controllers/dms");
 
 router.post("/", dmsCtrl.createChat)
+router.get("/currentUser", dmsCtrl.getChatsForCurrentUser)
 
 module.exports = router
