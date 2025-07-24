8a4e10c994f15b3a680cf94da24cc6d8efac5417
sara-lai <shihualai@gmail.com>
2025-07-24 18:04:24 +0800

    small updates from running locally

--- controllers/marketplaceCtrl.js
+++ controllers/marketplaceCtrl.js
@@ -1,23 +1,25 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../models/users")
+const Marketplace = require("../daos/marketplaceDao")
 const marketplaceModel = require("../models/marketplaceModel");
 
 // Fetch all marketplace listings
 const allList = async (req, res) => {
   
   try {
-    const listings = await marketplaceModel.getAllListing(req.query);
-    res.json({ message: "GET Successful", listings})
+    const listings = await marketplaceModel.getAllListing();
+    res.json({ listings: listings})
   } catch (error) {
     console.error("Error fetching marketplace listings:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 }
+
 async function viewListing(req, res){
   try {
     const listingId = req.params.id
-    const item = await marketplaceModel.findById(listingId)
+    const item = await Marketplace.findById(listingId)
     res.json({ item });
   } catch (error) {
     console.error("Error fetching marketplace item:", error);
@@ -81,7 +83,7 @@ const updateListing = async (req, res) => {
     const userId = req.auth.userId
     const itemData = { ...req.body, userId }
 
-    const listing = await marketplaceModel.findById(itemData._id)
+    const listing = await Marketplace.findById(itemData._id)
     const listingUpdated = await Marketplace.findByIdAndUpdate(listing._id, itemData, { new: true })
     res.json({ listing: listingUpdated })
 
@@ -97,7 +99,7 @@ const  deleteListing= async (req, res) => {
   try {
     const userId = req.auth.userId
     const listingId = req.params.id
-    const listing = await marketplaceModel.findById(listingId)
+    const listing = await Marketplace.findById(listingId)
     
     if (listing.user_id === userId){ 
     const deleted = await Marketplace.findByIdAndDelete(listingId)
